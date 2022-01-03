b37440496e2fe5f7bb7a9c02249c210f301e5240
Jose Guillermo <jguillermo@outlook.com>
2022-01-02 22:03:13 -0500

    rename wordir update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,2 @@
 #!/bin/sh -l
-export
-echo $GITHUB_WORKFLOW
-find framework/features -name '*.feature' | xargs cat | (! grep -n --color '#')
+find $GITHUB_WORKSPACE/framework/features -name '*.feature' | xargs cat | (! grep -n --color '#')
